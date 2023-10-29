973840ac0cdae5fd9b407fa11a03c643337f9089
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-29 07:25:34 +0000

    タイトル以外完成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,16 @@
 class Book < ApplicationRecord
 
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :profile_image #ここ本の画像に代わるかも
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{ maximum:200 }
 
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
